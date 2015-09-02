ccd58912eaec095d3b97e3756f67c72eb706ca42
Simon <simonthechain@gmail.com>
2015-09-02 00:43:32 -0400

    Update_Tic-tac-toe_3.1
    Functional version

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -15,96 +15,92 @@ def print_board():
 
 # This lets the users choose where to place their mark
 # and verifies if that space is already occupied
-
-
 	
 def player_input(x_or_o):	
-	player = raw_input("\nPlace your %s:\n> " % x_or_o)
-	if player == "a1":
-		if a1 == ["X"] or a1 == ["O"]:
-			print "\nAlready occupied, choose again.\n"
+	while True:
+		player = raw_input("\nPlace your %s:\n> " % x_or_o)
+		if player == "a1":
+			if a1 == ["X"] or a1 == ["O"]:
+				print "\nAlready occupied, choose again.\n"
+			else:
+				a1.pop()
+				a1.append(x_or_o)
+				return a1
+		elif player == "a2":
+			if a2 == ["X"] or a2 == ["O"]:
+				print "\nAlready occupied, choose again.\n"
+			else:
+				a2.pop()
+				a2.append(x_or_o)
+				return a2
+		elif player == "a3":
+			if a3 == ["X"] or a3 == ["O"]:
+				print "\nAlready occupied, choose again.\n"
+			else:
+				a3.pop()
+				a3.append(x_or_o)
+				return a3
+		elif player == "b1":
+			if b1 == ["X"] or b1 == ["O"]:
+				print "\nAlready occupied, choose again.\n"
+			else:
+				b1.pop()
+				b1.append(x_or_o)
+				return b1
+		elif player == "b2":
+			if b2 == ["X"] or b2 == ["O"]:
+				print "\nAlready occupied, choose again.\n"
+			else:
+				b2.pop()
+				b2.append(x_or_o)
+				return b2
+		elif player == "b3":
+			if b3 == ["X"] or b3 == ["O"]:
+				print "\nAlready occupied, choose again.\n"
+			else:
+				b3.pop()
+				b3.append(x_or_o)
+				return b3
+		elif player == "c1":
+			if c1 == ["X"] or c1 == ["O"]:
+				print "\nAlready occupied, choose again.\n"
+			else:
+				c1.pop()
+				c1.append(x_or_o)
+				return c1
+		elif player == "c2":
+			if c2 == ["X"] or c2 == ["O"]:
+				print "\nAlready occupied, choose again.\n"
+			else:
+				c2.pop()
+				c2.append(x_or_o)
+				return c2
+		elif player == "c3":
+			if c3 == ["X"] or c3 == ["O"]:
+				print "\nAlready occupied, choose again.\n"
+			else:
+				c3.pop()
+				c3.append(x_or_o)
+				return c3
 		else:
-			a1.pop()
-			a1.append(x_or_o)
-			return a1
-	elif player == "a2":
-		if a2 == ["X"] or a2 == ["O"]:
-			print "\nAlready occupied, choose again.\n"
-		else:
-			a2.pop()
-			a2.append(x_or_o)
-			return a2
-	elif player == "a3":
-		if a3 == ["X"] or a3 == ["O"]:
-			print "\nAlready occupied, choose again.\n"
-		else:
-			a3.pop()
-			a3.append(x_or_o)
-			return a3
-	elif player == "b1":
-		if b1 == ["X"] or b1 == ["O"]:
-			print "\nAlready occupied, choose again.\n"
-		else:
-			b1.pop()
-			b1.append(x_or_o)
-			return b1
-	elif player == "b2":
-		if b2 == ["X"] or b2 == ["O"]:
-			print "\nAlready occupied, choose again.\n"
-		else:
-			b2.pop()
-			b2.append(x_or_o)
-			return b2
-	elif player == "b3":
-		if b3 == ["X"] or b3 == ["O"]:
-			print "\nAlready occupied, choose again.\n"
-		else:
-			b3.pop()
-			b3.append(x_or_o)
-			return b3
-	elif player == "c1":
-		if c1 == ["X"] or c1 == ["O"]:
-			print "\nAlready occupied, choose again.\n"
-		else:
-			c1.pop()
-			c1.append(x_or_o)
-			return c1
-	elif player == "c2":
-		if c2 == ["X"] or c2 == ["O"]:
-			print "\nAlready occupied, choose again.\n"
-		else:
-			c2.pop()
-			c2.append(x_or_o)
-			return c2
-	elif player == "c3":
-		if c3 == ["X"] or c3 == ["O"]:
-			print "\nAlready occupied, choose again.\n"
-		else:
-			c3.pop()
-			c3.append(x_or_o)
-			return c3
-	else:
-		print "\nUnrecognized input."
-# Debug
-		quit()
+			print "\nUnrecognized input."
 
 # This checks if the winning conditions have been met
 def win_check(x_or_o):
-	if (a1 == x_or_o and a2 == x_or_o and a3 == x_or_o) or \
-(b1 == x_or_o and b2 == x_or_o and b3 == x_or_o) or \
-(c1 == x_or_o and c2 == x_or_o and c3 == x_or_o) or \
-(a1 == x_or_o and b1 == x_or_o and c1 == x_or_o) or \
-(a2 == x_or_o and b2 == x_or_o and c2 == x_or_o) or \
-(a3 == x_or_o and b3 == x_or_o and c3 == x_or_o) or \
-(a1 == x_or_o and b2 == x_or_o and c3 == x_or_o) or \
-(a3 == x_or_o and b2 == x_or_o and c1 == x_or_o):
+	if (a1 == [x_or_o] and a2 == [x_or_o] and a3 == [x_or_o]) or \
+(b1 == [x_or_o] and b2 == [x_or_o] and b3 == [x_or_o]) or \
+(c1 == [x_or_o] and c2 == [x_or_o] and c3 == [x_or_o]) or \
+(a1 == [x_or_o] and b1 == [x_or_o] and c1 == [x_or_o]) or \
+(a2 == [x_or_o] and b2 == [x_or_o] and c2 == [x_or_o]) or \
+(a3 == [x_or_o] and b3 == [x_or_o] and c3 == [x_or_o]) or \
+(a1 == [x_or_o] and b2 == [x_or_o] and c3 == [x_or_o]) or \
+(a3 == [x_or_o] and b2 == [x_or_o] and c1 == [x_or_o]):
 		print "\n%s wins!\n" % x_or_o
 		quit()
 		
 # The program starts here
 print_board()
-input_loop = True
-while input_loop == True:
+while True:
 	player_input("X")
 	print_board()
 	win_check("X")
